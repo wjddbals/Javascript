16273510569118993a37ca45600832df0b41f396
FULLSTACK <jumin0220@naver.com>
2023-01-27 12:01:15 +0900

    함수 화살표 선언 방식

---
+++ js/08func.js
@@ -0,0 +1,51 @@
+function computeLeapYear (year) {
+    let result='평년입니다';
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        result =`윤년입니다<br>`;
+    }
+    return `${year}년은 ${result}`;
+}
+
+
+
+function showStarPyramid(floor) {
+    let result = '';
+    for (let j = 1; j <= floor; ++j) { //층 반복문
+        for (let i = 1; i <= j; ++i) { //별 반복문
+            result += `* `;
+        }
+        result += `<br> `;
+    }
+document.write(result);
+}
+
+function showAMPM(time) {
+    let result =`오후입니다`;
+    hour = time[0] + time[1];
+
+    if (hour < 12) {
+        result = `오전입니다`;}
+    return `${hour}시는 ${result}`;
+
+}
+
+
+function computeSungJuk(kor,eng,mat){
+    let tot =0; //let tot =0,avg =0, grd=`가`;다중변수 선언
+    let avg =0;
+    let grd =`가`;
+
+    tot =kor +eng+ mat;
+    avg =tot/3;
+
+    switch (parseInt(avg/10)){
+        case 10:
+        case 9:result='수';break;
+        case 8:result='우';break;
+        case 7:result='미';break;
+        case 6:result='양';break;
+
+    }
+    document.write(`총점은 ${tot},평균은 ${avg} 학점은 ${grd}입니다`);
+}
